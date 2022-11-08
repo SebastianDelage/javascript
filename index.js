2e75485f17010cc267252794eb582a035e899226
sejude <108374679+sejude@users.noreply.github.com>
2022-11-08 13:55:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ let nombre = prompt("Ingrese su nombre");
 let valorTotal = 0;
 alert("Bienvenido al carrito " + nombre);
 let continuar = confirm("desea continuar?");
-const iva = 0.21; 
+const carrito = []
+const iva = 0.21;
+
+class Producto{
+    constructor(nombre,precio,id){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+    }
+
+    
+}
+
+const producto1 = new Producto("pantalon",1000,001);
+const producto2 = new Producto("remera",2000,002);
+const producto3 = new Producto("campera",9000,003);
+const producto4 = new Producto("buso",3000,004);
+
+const BBDD =[producto1,producto2,producto3,producto4]
+
+BBDD.forEach((el) => {
+    console.log(el)
+});
+
 while(continuar){
-    let productos = Number(prompt("Seleccione un producto \n 1-pantalon 2-remera 3-campera"));
-    if (productos > 3){
+    let productos = Number(prompt("Seleccione un producto \n 1-pantalon 2-remera 3-campera 4-buso"));
+    if (productos > 4){
         alert("numero invalido")
     }
     switch (productos){
@@ -14,21 +37,31 @@ while(continuar){
             cantidad = prompt("Cuantos va a llevar?");
             valorTotal = 1000*cantidad;
             console.log(valorTotal);
+            carrito.push(BBDD[0]);
             break;
         case 2:    
             cantidad = prompt("Cuantos va a llevar?");
             valorTotal = 2000*cantidad;
             console.log(valorTotal);
+            carrito.push(BBDD[1]);
              break;
         case 3:
             cantidad = prompt("Cuantos va a llevar?");
             valorTotal = 9000*cantidad;
             console.log(valorTotal);
+            carrito.push(BBDD[2]);
+            break;
+        case 4:    
+             cantidad = prompt("Cuantos va a llevar?");
+             valorTotal = 3000*cantidad;
+            console.log(valorTotal);
+            carrito.push(BBDD[3]);
             break;
     }
     continuar = confirm("desea continuar?"); 
 } 
 
+console.log(carrito)
 
 totalIva(iva,valorTotal); 
 
